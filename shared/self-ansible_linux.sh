86c1d68e293be1812eb5b15f6158652db06886cd
Yuriel-V <emberbec@gmail.com>
2021-03-21 23:38:27 -0300

    Change global stuff
    - Add Terraria VM
    - Validate global role
    - Change global and self-ansible scripts
    - Also change Ansible inventory and Vagrantfile

--- shared/self-ansible_linux.sh
+++ shared/self-ansible_linux.sh
@@ -1,5 +1,9 @@
+#!/usr/bin/bash
+
+# 0: type; 1 onwards: extra vars
 type=$1
-selfip=`hostname -I | grep "172\.16\.[0-9]*\.[0-9]*"`
+extra_vars=$2
+selfip=`hostname -I | grep -o "172\.16\.[0-9]*\.[0-9]*"`
 selfid=`cat /etc/os-release | grep ID= | head -n 1 | grep -o "[^=]*$" | grep -o "[^\"]*"`
 ssh -i /root/id_ansible -o StrictHostKeyChecking=no vagrant@172.16.0.100 \
-"ansible-playbook /shared/global/ansible/start.yml -i inv --limit \"$selfip,\" --tags \"$type\""
+"ansible-playbook /shared/global/ansible/start.yml -v -i /shared/global/ansible/inv --limit \"$selfip,\" --tags \"$type\" --extra-vars \"$extra_vars\""
--- ubuntu/shared/global.sh
+++ ubuntu/shared/global.sh
@@ -3,3 +3,6 @@ echo "-> (global.sh) Copying Ansible key to authorized keys"
 cat /shared/global/id_ansible.pub >> /home/vagrant/.ssh/authorized_keys
 cp /shared/global/id_ansible /root/
 chmod 0700 /root/id_ansible
+
+echo "-> (global.sh) Refreshing apt-cache"
+apt-get update
