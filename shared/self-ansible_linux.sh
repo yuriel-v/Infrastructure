9e79c41f4ba26fbb7be8ec4100a9517722c83456
Yuriel-V <emberbec@gmail.com>
2021-04-28 12:47:17 -0300

    General tweaks
    - Below changes for global.sh
      - Zipped Ansible SSH key pair into one zipped file
      - Install unzip from shell
      - Unzip Ansible key pair to /root/ to avoid permissions issues
    - Remove verbose mode from self-ansible script
    - Update .gitignore

--- shared/self-ansible_linux.sh
+++ shared/self-ansible_linux.sh
@@ -6,4 +6,4 @@ extra_vars=$2
 selfip=`hostname -I | grep -o "172\.16\.[0-9]*\.[0-9]*"`
 selfid=`cat /etc/os-release | grep ID= | head -n 1 | grep -o "[^=]*$" | grep -o "[^\"]*"`
 ssh -i /root/id_ansible -o StrictHostKeyChecking=no vagrant@172.16.0.100 \
-"ansible-playbook /shared/global/ansible/start.yml -v -i /shared/global/ansible/inv --limit \"$selfip,\" --tags \"$type\" --extra-vars \"$extra_vars\""
+"ansible-playbook /shared/global/ansible/start.yml -i /shared/global/ansible/inv --limit \"$selfip,\" --tags \"$type\" --extra-vars \"$extra_vars\""
--- ubuntu/shared/global.sh
+++ ubuntu/shared/global.sh
@@ -1,8 +1,13 @@
 # Global Ubuntu configs script
-echo "-> (global.sh) Copying Ansible key to authorized keys"
-cat /shared/global/id_ansible.pub >> /home/vagrant/.ssh/authorized_keys
-cp /shared/global/id_ansible /root/
-chmod 0700 /root/id_ansible
-
 echo "-> (global.sh) Refreshing apt-cache"
 apt-get update
+
+echo "-> (global.sh) Installing unzip"
+apt-get install unzip
+
+echo "-> (global.sh) Unzipping Ansible keys"
+unzip /shared/global/id_ansible.zip -d /root/
+
+echo "-> (global.sh) Copying Ansible key to authorized keys"
+cat /root/id_ansible.pub >> /home/vagrant/.ssh/authorized_keys
+chmod 0700 /root/id_ansible
