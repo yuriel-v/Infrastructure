56d1b60665f431e2d983fb9a4fef5a8d1f12e4a8
yuriel-v <emberbec@gmail.com>
2021-04-30 02:45:51 +0000

    Restructure datacenter, tweak some code
    - Edit scripts and other files to match new datacenter folder structure
    - Change Ansible inventory format to YAML style (previously INI)
    - Change self-ansible script host part to reflect above change
    - Simplify dev.yml playbook
    - Change dev-master role to install VS Code extensions
    - Minor code tweaks

---
+++ shared/linux-scripts/self-ansible_linux.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# 0: type; 1 onwards: extra vars
+type=$1
+host=$2
+extra_vars=$3
+#selfip=`hostname -I | grep -o "172\.16\.[0-9]*\.[0-9]*"`
+#selfid=`cat /etc/os-release | grep ID= | head -n 1 | grep -o "[^=]*$" | grep -o "[^\"]*"`
+ssh -i /root/id_ansible -o StrictHostKeyChecking=no vagrant@172.16.0.100 \
+"ansible-playbook /shared/global/ansible/start.yml -i /shared/global/ansible/inv --limit \"$host,\" --tags \"$type\" --extra-vars \"$extra_vars\""
